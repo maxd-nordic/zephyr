0227244359362da616c7a91a9f29671ba4d0f39b
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2023-07-07 15:23:55 +0200

    dap: dap_swj_pins: implement wait
    Signed-off-by: Maximilian Deubel <maximilian.deubel@nordicsemi.no>

--- subsys/dap/cmsis_dap.c
+++ subsys/dap/cmsis_dap.c
@@ -226,6 +226,7 @@ static uint16_t dap_swj_pins(struct dap_context *const ctx,
 	uint8_t select = request[1];
 	uint32_t wait = sys_get_le32(&request[2]);
 	uint8_t state;
+	int64_t timout_ticks = k_uptime_ticks() + (CONFIG_SYS_CLOCK_TICKS_PER_SEC / 1000000 * wait);
 
 	if (!atomic_test_bit(&ctx->state, DAP_STATE_CONNECTED)) {
 		LOG_ERR("DAP device is not connected");
@@ -238,10 +239,16 @@ static uint16_t dap_swj_pins(struct dap_context *const ctx,
 		api->swdp_set_pins(ctx->swdp_dev, select, value);
 	}
 
-	/* TODO: implement wait */
-	api->swdp_get_pins(ctx->swdp_dev, &state);
-	LOG_ERR("select 0x%02x, value 0x%02x, wait %u, state 0x%02x",
-		select, value, wait, state);
+	do
+	{
+		api->swdp_get_pins(ctx->swdp_dev, &state);
+		LOG_INF("select 0x%02x, value 0x%02x, wait %u, state 0x%02x",
+			select, value, wait, state);
+		if ((value & select) == (state & select)) {
+			LOG_DBG("swdp_get_pins succeeded before timeout");
+			break;
+		}
+	} while (k_uptime_ticks() - timout_ticks > 0);
 
 	response[0] = state;
 
